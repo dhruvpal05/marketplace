f651a792a4c251822edfdbb157ce35338117587b
dhruvpal05 <dhruv416pal@gmail.com>
2023-12-11 11:23:05 +0530

    added vercel

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -133,3 +133,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'build/static')]
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_URL = 'static/'
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
